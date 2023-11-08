d089b4ac754d36921338ab76fa8d4facb73fd0f2
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-11-08 18:38:15 +0100

    Added navigation with styling

---
+++ src/components/navigation/MobileNavigation.jsx
@@ -0,0 +1,12 @@
+import NavLinks from "./NavLinks";
+
+const MobileNavigation = () => {
+  return (
+    <>
+      <img src="../../assets/holidaze_logo_black.png" alt="Logo" />
+      <NavLinks />
+    </>
+  );
+};
+
+export default MobileNavigation;
---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import NavLinks from "./NavLinks";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <img src="../../../public/assets/holidaze_logo_black.png" alt="Logo" />
+      <NavLinks />
+    </nav>
+  );
+};
+
+export default Navigation;
