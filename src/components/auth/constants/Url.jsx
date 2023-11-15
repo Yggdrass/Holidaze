f05dd854523ca66401ef84ef453e0832665c5849
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-11-15 19:56:50 +0100

    Created a venues page, with a search input and aan api call to fetch all venues.

--- src/components/auth/constants/Url.jsx
+++ src/components/auth/constants/Url.jsx
@@ -3,3 +3,5 @@ export const Url = "https://api.noroff.dev/api/v1";
 export const RegisterUrl = Url + "/holidaze/auth/register";
 
 export const LoginUrl = Url + "/holidaze/auth/login";
+
+export const All_Venues_Url = Url + "/holidaze/venues";
